8a6f03cf7e64dbfa2bcef1bd8e0a711584ace7fc
sajan shrestha <sajanshrestha@sajan-shrestha-2.local>
2024-06-29 22:28:10 +0545

    added nltk packages in local

---
+++ resume_screening/ml_model/download_nltk.py
@@ -0,0 +1,4 @@
+import nltk
+nltk.download('punkt', download_dir='./nltk_data')
+nltk.download('averaged_perceptron_tagger', download_dir='./nltk_data')
+nltk.download('wordnet', download_dir='./nltk_data')
